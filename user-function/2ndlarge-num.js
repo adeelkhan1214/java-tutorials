6bc8b0948c719f8518ec9342db1b371df6ffca77
adeel <adeelabbaskhan5@gmail.com>
2025-05-16 19:23:54 +0500

    add_change

--- user-function/2ndlarge-num.js
+++ user-function/2ndlarge-num.js
@@ -1,6 +1,21 @@
 
+// const arr = [54,2, 3, 4, 4, 5, 5, 56, 4];
+
+// const secondLast = arr[arr.length - 2];
+
+// console.log("Second last element is:", secondLast);
 const arr = [2, 3, 4, 4, 5, 5, 55, 56, 4];
 
-const secondLast = arr[arr.length - 2];
+let largest = arr[0];
+let secondLargest = arr[0];
 
-console.log("Second last element is:", secondLast);
+for (let i = 1; i < arr.length; i++) {
+  if (arr[i] > largest) {
+    secondLargest = largest;
+    largest = arr[i];
+  } else if (arr[i] > secondLargest && arr[i] !== largest) {
+    secondLargest = arr[i];
+  }
+}
+
+console.log("Second largest number is:", secondLargest);
